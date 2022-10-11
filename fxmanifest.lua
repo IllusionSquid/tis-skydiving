7310f89fba58a311862e1fc0ab72d9ce822b7c7b
IllusionSquid <theillusionsquid@gmail.com>
2022-10-11 13:52:09 +0200

    Remove unneccesary ui

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -24,15 +24,4 @@ server_scripts {
 	'config.lua'
 }
 
-ui_page {
-	'html/ui.html'
-}
-
-files {
-	'html/ui.html',
-	'html/css/main.css',
-	'html/js/app.js',
-	'html/fonts/*.ttf',
-}
-
 dependency 'menuv'
